7ec988135a068a948746f46f874e081a3e2d98cd
ivan1299 <ivan.mi1299@gmail.com>
2022-01-29 01:40:44 -0300

    clase2

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,32 @@
+export const ItemListContainer = () => {
+
+    return (
+        <div className="catalog">
+        <div className="card">
+            <div className="container">
+                <h4><b>New Balance</b></h4> 
+                <p>13 Modelos</p> 
+            </div>
+        </div>
+        <div className="card">
+            <div className="container">
+                <h4><b>Puma</b></h4> 
+                <p>30 Moldeos</p> 
+            </div>
+        </div>
+        <div className="card">
+            <div className="container">
+                <h4><b>Adidas</b></h4> 
+                <p>28 Modelos</p> 
+            </div>
+        </div>
+        <div className="card">
+            <div className="container">
+                <h4><b>Nike</b></h4> 
+                <p>45 Modelos</p> 
+            </div>
+        </div>
+        </div>
+      );
+
+};
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+export const NavBar = () => {
+
+    return (
+        <div className="navBar">
+            <h1>Logo</h1>
+
+            <ul>
+                <li>Botines</li>
+                <li>Running</li>
+                <li>Zapatillas</li>
+                <li>Todo</li>
+            </ul>
+        </div>
+      );
+
+};
